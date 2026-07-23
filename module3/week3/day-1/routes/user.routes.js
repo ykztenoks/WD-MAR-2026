993cda90d81d84b2cc3ba4fa81f8bce21a169bac
ykztenoks <danielskonetzky7@gmail.com>
2026-07-23 14:35:51 -0300

    lesson done auth 🥸

--- module3/week3/day-1/routes/user.routes.js
+++ module3/week3/day-1/routes/user.routes.js
@@ -1,6 +1,8 @@
 import express from "express"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import isAuth from "../middlewares/isAuth.middleware.js"
 
 const router = express.Router()
 
@@ -19,13 +21,14 @@ router.post("/signup", async (req, res) => {
     const { email, username, password } = req.body
 
     if (!email || !password || !username) {
-      res.status(400).json({ message: "Please provide all fields" })
+      return res.status(400).json({ message: "Please provide all fields" })
     }
 
     const passwordRegex =
       /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
+
     if (!passwordRegex.test(password)) {
-      res.status(400).json({
+      return res.status(400).json({
         message:
           "Password must be 8 characters long and contain one uppercase and one lowercase character, a number, and a special character.",
       })
@@ -34,7 +37,7 @@ router.post("/signup", async (req, res) => {
 
     const foundUser = await User.findOne({ $or: [{ email }, { username }] })
     if (foundUser) {
-      res.status(409).json({ message: "User already exists" })
+      return res.status(409).json({ message: "User already exists" })
     }
 
     const salts = await bcrypt.genSalt(10)
@@ -53,4 +56,51 @@ router.post("/signup", async (req, res) => {
     res.status(500).json(error.message)
   }
 })
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, username, password } = req.body
+
+    if ((!email && !username) || !password) {
+      return res
+        .status(400)
+        .json({ message: "please provide email or username and password" })
+    }
+
+    const foundUser = await User.findOne({ $or: [{ email }, { username }] })
+
+    if (!foundUser) {
+      return res.status(404).json({ message: "This user does not exist" })
+    }
+
+    const passwordCheck = await bcrypt.compare(password, foundUser.password)
+
+    if (!passwordCheck) {
+      return res.status(401).json({ message: "Password incorrect" })
+    }
+
+    const token = await jwt.sign(
+      {
+        email: foundUser.email,
+        username: foundUser.username,
+        _id: foundUser._id,
+      },
+      process.env.TOKEN_SECRET,
+      { algorithm: "HS256", expiresIn: "1h" },
+    )
+
+    res
+      .status(200)
+      .json({ message: "Logged in succesfully", token, user: foundUser })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+})
+
+router.get("/verify", isAuth, (req, res) => {
+  console.log(req.user)
+  res.status(200).json({ user: req.user })
+})
+
 export default router
